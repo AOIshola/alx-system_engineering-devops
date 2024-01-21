2a72ba9bfe276531709d0221db94b757479f9397
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-21 05:01:46 -0800

    write puppet manifests

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,15 +1,9 @@
 # kills a process named killmenow
 
 exec { 'killmenow':
-  command     => '/usr/bin/pkill -f killmenow',
+  command     => 'pkill -f killmenow',
+  path        => ['/usr/bin', '/usr/sbin', '/bin'],
+  provider    => 'shell',
   onlyif      => '/usr/bin/pgrep -f killmenow',
-  refreshonly => true,
   logoutput   => true,
-  subscribe   => File['/alx-system_engineering-devops/0x0A-configuration_management/killmenow'],
-}
-
-file { '/alx-system_engineering-devops/0x0A-configuration_management/killmenow':
-  ensure  => file,
-  content => "#!/bin/bash\nwhile [[ true ]]; do sleep 2; done\n",
-  mode    => '0755',  # Make the script executable
 }
