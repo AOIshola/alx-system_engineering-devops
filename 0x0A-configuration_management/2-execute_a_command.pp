c063d86bceb585fcd70e06ac7ad4637e11d7dda2
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-21 05:07:41 -0800

    write puppet manifests

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,9 @@
 # kills a process named killmenow
 
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => ['/usr/bin', '/usr/sbin', '/bin'],
-  provider    => 'shell',
-  onlyif      => '/usr/bin/pgrep -f killmenow',
-  logoutput   => true,
+  command   => 'pkill -f killmenow',
+  path      => ['/usr/bin', '/usr/sbin', '/bin'],
+  provider  => 'shell',
+  onlyif    => '/usr/bin/pgrep -f killmenow',
+  logoutput => true,
 }
