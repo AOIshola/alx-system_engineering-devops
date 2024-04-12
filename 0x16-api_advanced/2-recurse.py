2e5934a1a5210723ab5245ea4766e2ea9cdb8805
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-04-11 18:18:58 -0700

    query Reddit API recursively

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,18 +8,19 @@ def recurse(subreddit, hot_list=[], after=None):
     """queries the Reddit API and returns a list containing
     the titles of all hot articles for a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    
+
     headers = {'User-Agent': 'MyBot/1.0'}
-    
+
     params = {'after': after} if after else {}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
-        
+
         for post in data['data']['children']:
             hot_list.append(post['data']['title'])
-        
+
         after = data['data'].get('after')
         if after:
             # Recursive call to fetch next page
