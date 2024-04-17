2a6aca2072f64e4f6e41459e171d83bf1b627d98
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-04-17 10:52:39 -0700

    automate webstack debugging with puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,12 +4,8 @@ $file='/var/www/html/wp-settings.php'
 $command='sed -i "s/phpp/php/"'
 
 exec { 'replace-phpp-with-php':
-    command => "${command} ${file}",
-    path    => ['/bin', '/usr/bin'],
-    onlyif  => 'find ${file}',
+  command => "${command} ${file}",
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'find ${file}',
 }
 
-#exec { 'Reload Apache':
-  #command => 'sudo service apache reload',
- # path    => ['/bin', '/usr/bin'],
-#}
